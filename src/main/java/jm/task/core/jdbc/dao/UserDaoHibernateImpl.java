09ad5ad311bfd860f377d5104cede8c96323a058
jalolovm <muhammadzalolov@gmail.com>
2020-10-20 18:29:30 +0300

    commit 20.10

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -34,7 +34,7 @@ public class UserDaoHibernateImpl implements UserDao {
         String sql = "DROP TABLE IF EXISTS users";
         try (Session session = Util.getSessionFactory().openSession()) {
             session.beginTransaction();
-            session.createNativeQuery(sql).addEntity(User.class).executeUpdate();
+            session.createNativeQuery(sql).executeUpdate();
             session.getTransaction().commit();
         }
     }
@@ -42,12 +42,15 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void saveUser(String name, String lastName, byte age) {
         User user = new User(name, lastName, age);
-
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             session.save(user);
-            session.getTransaction().commit();
+            transaction.commit();
+        } catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
         }
     }
 
